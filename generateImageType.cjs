244f50a78ce812f1197bb9ebcde5a47787b69750
flamintune <1853995253@qq.com>
2024-01-23 18:01:03 +0900

    fix: cancel dynamic load

--- generateImageType.cjs
+++ generateImageType.cjs
@@ -3,27 +3,32 @@ const path = require('path');
 
 const imagesDir = path.join(__dirname, 'src/assets');
 const outputPath = path.join(__dirname, 'src/types/imagePaths.ts');
-
 // Read the directory contents
 fs.readdir(imagesDir, (err, files) => {
   if (err) {
     console.error('Error reading the images directory', err);
     return;
   }
-
+  const filterFiles = files
+    .filter(file => /\.(jpe?g|png|gif)$/i.test(file))
   const imageFiles = files
     .filter(file => /\.(jpe?g|png|gif)$/i.test(file)) // Filter for image files
     .map(file => `'/src/assets/${file}'`) // Wrap in quotes
     .join(' | '); // Join into a TypeScript union type
 
   const typeString = `export type ImagePath = ${imageFiles};\n`;
-  const enumImageFiles = files.filter(file => /\.(jpe?g|png|gif)$/i.test(file)) // Filter for image files
+  const enumImageFiles = filterFiles // Filter for image files
     .map(file => `'/src/assets/${file}'`) // Wrap in quotes
     .join(',\n'); // Join into a TypeScript union type
   const enumString = `export enum ImageEnum {${enumImageFiles}
-  }`
+  }\n`
+  const importImages = filterFiles
+    .map((file, idx) => `import p${idx} from "/src/assets/${file}"`)
+    .join('\n')
+  const exportImages = `\nconst pictures = [${filterFiles.map((_, idx) => 'p' + idx).join(',')}]\n
+  export {pictures}`
   // Write the type to a file
-  fs.writeFile(outputPath, typeString+enumString, (err) => {
+  fs.writeFile(outputPath, typeString + enumString + importImages + exportImages, (err) => {
     if (err) {
       console.error('Error writing the imagePaths.ts file', err);
     } else {
--- src/types/imagePaths.ts
+++ src/types/imagePaths.ts
@@ -19,3 +19,25 @@ export enum ImageEnum {'/src/assets/IMG_20240114_113348.jpg',
 '/src/assets/IMG_20240122_190205.jpg',
 '/src/assets/IMG_20240123_083327.jpg'
   }
+import p0 from "/src/assets/IMG_20240114_113348.jpg"
+import p1 from "/src/assets/IMG_20240114_120652.jpg"
+import p2 from "/src/assets/IMG_20240115_090727.jpg"
+import p3 from "/src/assets/IMG_20240115_122818.jpg"
+import p4 from "/src/assets/IMG_20240115_125004.jpg"
+import p5 from "/src/assets/IMG_20240116_091022.jpg"
+import p6 from "/src/assets/IMG_20240117_104934.jpg"
+import p7 from "/src/assets/IMG_20240117_120650.jpg"
+import p8 from "/src/assets/IMG_20240117_191557.jpg"
+import p9 from "/src/assets/IMG_20240118_090824.jpg"
+import p10 from "/src/assets/IMG_20240118_135544.jpg"
+import p11 from "/src/assets/IMG_20240118_175711.jpg"
+import p12 from "/src/assets/IMG_20240118_184528.jpg"
+import p13 from "/src/assets/IMG_20240119_125512.jpg"
+import p14 from "/src/assets/IMG_20240121_115813.jpg"
+import p15 from "/src/assets/IMG_20240122_121408.jpg"
+import p16 from "/src/assets/IMG_20240122_123346.jpg"
+import p17 from "/src/assets/IMG_20240122_190205.jpg"
+import p18 from "/src/assets/IMG_20240123_083327.jpg"
+const pictures = [p0,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18]
+
+  export {pictures}
