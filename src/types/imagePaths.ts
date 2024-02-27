beb9ef56c69ca988fb6bac8da9d15c62a9d42fc3
flamintune <1853995253@qq.com>
2024-02-27 23:07:23 +0900

    feat 更新照片

--- src/types/imagePaths.ts
+++ src/types/imagePaths.ts
@@ -1,23 +1,149 @@
-export type ImagePath = '/src/assets/IMG_20240114_113348.jpg' | '/src/assets/IMG_20240114_120652.jpg' | '/src/assets/IMG_20240115_090727.jpg' | '/src/assets/IMG_20240115_122818.jpg' | '/src/assets/IMG_20240115_125004.jpg' | '/src/assets/IMG_20240116_091022.jpg' | '/src/assets/IMG_20240117_104934.jpg' | '/src/assets/IMG_20240117_120650.jpg' | '/src/assets/IMG_20240117_191557.jpg' | '/src/assets/IMG_20240118_090824.jpg' | '/src/assets/IMG_20240118_135544.jpg' | '/src/assets/IMG_20240118_175711.jpg' | '/src/assets/IMG_20240118_184528.jpg' | '/src/assets/IMG_20240119_125512.jpg' | '/src/assets/IMG_20240121_115813.jpg' | '/src/assets/IMG_20240122_121408.jpg' | '/src/assets/IMG_20240122_123346.jpg' | '/src/assets/IMG_20240122_190205.jpg' | '/src/assets/IMG_20240123_083327.jpg';
+export type ImagePath = '/src/assets/IMG_20240114_113348.jpg' | '/src/assets/IMG_20240114_120652.jpg' | '/src/assets/IMG_20240115_090727.jpg' | '/src/assets/IMG_20240115_125004.jpg' | '/src/assets/IMG_20240116_091022.jpg' | '/src/assets/IMG_20240117_104934.jpg' | '/src/assets/IMG_20240117_120650.jpg' | '/src/assets/IMG_20240117_191557.jpg' | '/src/assets/IMG_20240118_090824.jpg' | '/src/assets/IMG_20240118_135544.jpg' | '/src/assets/IMG_20240118_175711.jpg' | '/src/assets/IMG_20240118_184528.jpg' | '/src/assets/IMG_20240119_125512.jpg' | '/src/assets/IMG_20240121_115813.jpg' | '/src/assets/IMG_20240122_121408.jpg' | '/src/assets/IMG_20240122_123346.jpg' | '/src/assets/IMG_20240122_190205.jpg' | '/src/assets/IMG_20240123_083327.jpg' | '/src/assets/IMG_20240124_120618.jpg' | '/src/assets/IMG_20240124_191337.jpg' | '/src/assets/IMG_20240125_191244.jpg' | '/src/assets/IMG_20240125_214158.jpg' | '/src/assets/IMG_20240126_122446.jpg' | '/src/assets/IMG_20240126_144209.jpg' | '/src/assets/IMG_20240127_112107.jpg' | '/src/assets/IMG_20240127_194324.jpg' | '/src/assets/IMG_20240129_084224.jpg' | '/src/assets/IMG_20240129_191636.jpg' | '/src/assets/IMG_20240130_123155.jpg' | '/src/assets/IMG_20240130_125135.jpg' | '/src/assets/IMG_20240131_191733.jpg' | '/src/assets/IMG_20240131_192818.jpg' | '/src/assets/IMG_20240201_122715.jpg' | '/src/assets/IMG_20240201_222236.jpg' | '/src/assets/IMG_20240202_184837.jpg' | '/src/assets/IMG_20240203_121156.jpg' | '/src/assets/IMG_20240204_093750.jpg' | '/src/assets/IMG_20240204_093755.jpg' | '/src/assets/IMG_20240204_113833.jpg' | '/src/assets/IMG_20240204_211637.jpg' | '/src/assets/IMG_20240204_213303.jpg' | '/src/assets/IMG_20240204_213448.jpg' | '/src/assets/IMG_20240204_213949.jpg' | '/src/assets/IMG_20240205_194916.jpg' | '/src/assets/IMG_20240206_191906.jpg' | '/src/assets/IMG_20240206_192456.jpg' | '/src/assets/IMG_20240208_140708.jpg' | '/src/assets/IMG_20240208_201554.jpg' | '/src/assets/IMG_20240209_121134.jpg' | '/src/assets/IMG_20240209_180401.jpg' | '/src/assets/IMG_20240209_180609.jpg' | '/src/assets/IMG_20240209_220702.jpg' | '/src/assets/IMG_20240211_085732.jpg' | '/src/assets/IMG_20240212_123306.jpg' | '/src/assets/IMG_20240212_184755.jpg' | '/src/assets/IMG_20240212_191829.jpg' | '/src/assets/IMG_20240213_081222.jpg' | '/src/assets/IMG_20240213_085032.jpg' | '/src/assets/IMG_20240213_093546.jpg' | '/src/assets/IMG_20240213_094500.jpg' | '/src/assets/IMG_20240213_131832.jpg' | '/src/assets/IMG_20240213_160017.jpg' | '/src/assets/IMG_20240213_181019.jpg' | '/src/assets/IMG_20240213_200410.jpg' | '/src/assets/IMG_20240215_102447.jpg' | '/src/assets/IMG_20240215_112606.jpg' | '/src/assets/IMG_20240215_141351.jpg' | '/src/assets/IMG_20240215_151427.jpg' | '/src/assets/IMG_20240215_161341.jpg' | '/src/assets/IMG_20240219_143018.jpg' | '/src/assets/IMG_20240223_191629.jpg' | '/src/assets/IMG_20240227_201948.jpg';
+export enum ImageEnum {'/src/assets/IMG_20240114_113348.jpg',
+'/src/assets/IMG_20240114_120652.jpg',
+'/src/assets/IMG_20240115_090727.jpg',
+'/src/assets/IMG_20240115_125004.jpg',
+'/src/assets/IMG_20240116_091022.jpg',
+'/src/assets/IMG_20240117_104934.jpg',
+'/src/assets/IMG_20240117_120650.jpg',
+'/src/assets/IMG_20240117_191557.jpg',
+'/src/assets/IMG_20240118_090824.jpg',
+'/src/assets/IMG_20240118_135544.jpg',
+'/src/assets/IMG_20240118_175711.jpg',
+'/src/assets/IMG_20240118_184528.jpg',
+'/src/assets/IMG_20240119_125512.jpg',
+'/src/assets/IMG_20240121_115813.jpg',
+'/src/assets/IMG_20240122_121408.jpg',
+'/src/assets/IMG_20240122_123346.jpg',
+'/src/assets/IMG_20240122_190205.jpg',
+'/src/assets/IMG_20240123_083327.jpg',
+'/src/assets/IMG_20240124_120618.jpg',
+'/src/assets/IMG_20240124_191337.jpg',
+'/src/assets/IMG_20240125_191244.jpg',
+'/src/assets/IMG_20240125_214158.jpg',
+'/src/assets/IMG_20240126_122446.jpg',
+'/src/assets/IMG_20240126_144209.jpg',
+'/src/assets/IMG_20240127_112107.jpg',
+'/src/assets/IMG_20240127_194324.jpg',
+'/src/assets/IMG_20240129_084224.jpg',
+'/src/assets/IMG_20240129_191636.jpg',
+'/src/assets/IMG_20240130_123155.jpg',
+'/src/assets/IMG_20240130_125135.jpg',
+'/src/assets/IMG_20240131_191733.jpg',
+'/src/assets/IMG_20240131_192818.jpg',
+'/src/assets/IMG_20240201_122715.jpg',
+'/src/assets/IMG_20240201_222236.jpg',
+'/src/assets/IMG_20240202_184837.jpg',
+'/src/assets/IMG_20240203_121156.jpg',
+'/src/assets/IMG_20240204_093750.jpg',
+'/src/assets/IMG_20240204_093755.jpg',
+'/src/assets/IMG_20240204_113833.jpg',
+'/src/assets/IMG_20240204_211637.jpg',
+'/src/assets/IMG_20240204_213303.jpg',
+'/src/assets/IMG_20240204_213448.jpg',
+'/src/assets/IMG_20240204_213949.jpg',
+'/src/assets/IMG_20240205_194916.jpg',
+'/src/assets/IMG_20240206_191906.jpg',
+'/src/assets/IMG_20240206_192456.jpg',
+'/src/assets/IMG_20240208_140708.jpg',
+'/src/assets/IMG_20240208_201554.jpg',
+'/src/assets/IMG_20240209_121134.jpg',
+'/src/assets/IMG_20240209_180401.jpg',
+'/src/assets/IMG_20240209_180609.jpg',
+'/src/assets/IMG_20240209_220702.jpg',
+'/src/assets/IMG_20240211_085732.jpg',
+'/src/assets/IMG_20240212_123306.jpg',
+'/src/assets/IMG_20240212_184755.jpg',
+'/src/assets/IMG_20240212_191829.jpg',
+'/src/assets/IMG_20240213_081222.jpg',
+'/src/assets/IMG_20240213_085032.jpg',
+'/src/assets/IMG_20240213_093546.jpg',
+'/src/assets/IMG_20240213_094500.jpg',
+'/src/assets/IMG_20240213_131832.jpg',
+'/src/assets/IMG_20240213_160017.jpg',
+'/src/assets/IMG_20240213_181019.jpg',
+'/src/assets/IMG_20240213_200410.jpg',
+'/src/assets/IMG_20240215_102447.jpg',
+'/src/assets/IMG_20240215_112606.jpg',
+'/src/assets/IMG_20240215_141351.jpg',
+'/src/assets/IMG_20240215_151427.jpg',
+'/src/assets/IMG_20240215_161341.jpg',
+'/src/assets/IMG_20240219_143018.jpg',
+'/src/assets/IMG_20240223_191629.jpg',
+'/src/assets/IMG_20240227_201948.jpg'
+  }
 import p0 from "/src/assets/IMG_20240114_113348.jpg"
 import p1 from "/src/assets/IMG_20240114_120652.jpg"
 import p2 from "/src/assets/IMG_20240115_090727.jpg"
-import p3 from "/src/assets/IMG_20240115_122818.jpg"
-import p4 from "/src/assets/IMG_20240115_125004.jpg"
-import p5 from "/src/assets/IMG_20240116_091022.jpg"
-import p6 from "/src/assets/IMG_20240117_104934.jpg"
-import p7 from "/src/assets/IMG_20240117_120650.jpg"
-import p8 from "/src/assets/IMG_20240117_191557.jpg"
-import p9 from "/src/assets/IMG_20240118_090824.jpg"
-import p10 from "/src/assets/IMG_20240118_135544.jpg"
-import p11 from "/src/assets/IMG_20240118_175711.jpg"
-import p12 from "/src/assets/IMG_20240118_184528.jpg"
-import p13 from "/src/assets/IMG_20240119_125512.jpg"
-import p14 from "/src/assets/IMG_20240121_115813.jpg"
-import p15 from "/src/assets/IMG_20240122_121408.jpg"
-import p16 from "/src/assets/IMG_20240122_123346.jpg"
-import p17 from "/src/assets/IMG_20240122_190205.jpg"
-import p18 from "/src/assets/IMG_20240123_083327.jpg"
-const pictures = [p0,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18]
+import p3 from "/src/assets/IMG_20240115_125004.jpg"
+import p4 from "/src/assets/IMG_20240116_091022.jpg"
+import p5 from "/src/assets/IMG_20240117_104934.jpg"
+import p6 from "/src/assets/IMG_20240117_120650.jpg"
+import p7 from "/src/assets/IMG_20240117_191557.jpg"
+import p8 from "/src/assets/IMG_20240118_090824.jpg"
+import p9 from "/src/assets/IMG_20240118_135544.jpg"
+import p10 from "/src/assets/IMG_20240118_175711.jpg"
+import p11 from "/src/assets/IMG_20240118_184528.jpg"
+import p12 from "/src/assets/IMG_20240119_125512.jpg"
+import p13 from "/src/assets/IMG_20240121_115813.jpg"
+import p14 from "/src/assets/IMG_20240122_121408.jpg"
+import p15 from "/src/assets/IMG_20240122_123346.jpg"
+import p16 from "/src/assets/IMG_20240122_190205.jpg"
+import p17 from "/src/assets/IMG_20240123_083327.jpg"
+import p18 from "/src/assets/IMG_20240124_120618.jpg"
+import p19 from "/src/assets/IMG_20240124_191337.jpg"
+import p20 from "/src/assets/IMG_20240125_191244.jpg"
+import p21 from "/src/assets/IMG_20240125_214158.jpg"
+import p22 from "/src/assets/IMG_20240126_122446.jpg"
+import p23 from "/src/assets/IMG_20240126_144209.jpg"
+import p24 from "/src/assets/IMG_20240127_112107.jpg"
+import p25 from "/src/assets/IMG_20240127_194324.jpg"
+import p26 from "/src/assets/IMG_20240129_084224.jpg"
+import p27 from "/src/assets/IMG_20240129_191636.jpg"
+import p28 from "/src/assets/IMG_20240130_123155.jpg"
+import p29 from "/src/assets/IMG_20240130_125135.jpg"
+import p30 from "/src/assets/IMG_20240131_191733.jpg"
+import p31 from "/src/assets/IMG_20240131_192818.jpg"
+import p32 from "/src/assets/IMG_20240201_122715.jpg"
+import p33 from "/src/assets/IMG_20240201_222236.jpg"
+import p34 from "/src/assets/IMG_20240202_184837.jpg"
+import p35 from "/src/assets/IMG_20240203_121156.jpg"
+import p36 from "/src/assets/IMG_20240204_093750.jpg"
+import p37 from "/src/assets/IMG_20240204_093755.jpg"
+import p38 from "/src/assets/IMG_20240204_113833.jpg"
+import p39 from "/src/assets/IMG_20240204_211637.jpg"
+import p40 from "/src/assets/IMG_20240204_213303.jpg"
+import p41 from "/src/assets/IMG_20240204_213448.jpg"
+import p42 from "/src/assets/IMG_20240204_213949.jpg"
+import p43 from "/src/assets/IMG_20240205_194916.jpg"
+import p44 from "/src/assets/IMG_20240206_191906.jpg"
+import p45 from "/src/assets/IMG_20240206_192456.jpg"
+import p46 from "/src/assets/IMG_20240208_140708.jpg"
+import p47 from "/src/assets/IMG_20240208_201554.jpg"
+import p48 from "/src/assets/IMG_20240209_121134.jpg"
+import p49 from "/src/assets/IMG_20240209_180401.jpg"
+import p50 from "/src/assets/IMG_20240209_180609.jpg"
+import p51 from "/src/assets/IMG_20240209_220702.jpg"
+import p52 from "/src/assets/IMG_20240211_085732.jpg"
+import p53 from "/src/assets/IMG_20240212_123306.jpg"
+import p54 from "/src/assets/IMG_20240212_184755.jpg"
+import p55 from "/src/assets/IMG_20240212_191829.jpg"
+import p56 from "/src/assets/IMG_20240213_081222.jpg"
+import p57 from "/src/assets/IMG_20240213_085032.jpg"
+import p58 from "/src/assets/IMG_20240213_093546.jpg"
+import p59 from "/src/assets/IMG_20240213_094500.jpg"
+import p60 from "/src/assets/IMG_20240213_131832.jpg"
+import p61 from "/src/assets/IMG_20240213_160017.jpg"
+import p62 from "/src/assets/IMG_20240213_181019.jpg"
+import p63 from "/src/assets/IMG_20240213_200410.jpg"
+import p64 from "/src/assets/IMG_20240215_102447.jpg"
+import p65 from "/src/assets/IMG_20240215_112606.jpg"
+import p66 from "/src/assets/IMG_20240215_141351.jpg"
+import p67 from "/src/assets/IMG_20240215_151427.jpg"
+import p68 from "/src/assets/IMG_20240215_161341.jpg"
+import p69 from "/src/assets/IMG_20240219_143018.jpg"
+import p70 from "/src/assets/IMG_20240223_191629.jpg"
+import p71 from "/src/assets/IMG_20240227_201948.jpg"
+const pictures = [p0,p1,p2,p3,p4,p5,p6,p7,p8,p9,p10,p11,p12,p13,p14,p15,p16,p17,p18,p19,p20,p21,p22,p23,p24,p25,p26,p27,p28,p29,p30,p31,p32,p33,p34,p35,p36,p37,p38,p39,p40,p41,p42,p43,p44,p45,p46,p47,p48,p49,p50,p51,p52,p53,p54,p55,p56,p57,p58,p59,p60,p61,p62,p63,p64,p65,p66,p67,p68,p69,p70,p71]
 
   export {pictures}
